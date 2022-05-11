2dea0aeb084419342d392e39de8f1450f3157e01
golovko <myblackturkey@gmail.com>
2022-05-11 06:59:00 +0300

    deserializerializuetsa v jsone

--- lab2/Json.py
+++ lab2/Json.py
@@ -1,6 +1,7 @@
 import types
 import inspect
 import marshal
+import importlib
 
 
 class Serializer:
@@ -38,7 +39,7 @@ class Serializer:
                 s.append(Serializer.dumps(i))
             return str(s)
         elif type(obj) is tuple:
-            l = []
+            l = list()
             for i in obj:
                 l.append(Serializer.dumps(i))
             s = tuple(l)
@@ -108,3 +109,80 @@ class Serializer:
 
         elif inspect.isclass(type(obj)):
             return Serializer.serialize_noniterable(obj)
+
+
+class Deserializer:
+
+    @staticmethod
+    def deserialize_function(obj: dict) -> types.FunctionType:
+        code: types.CodeType = marshal.loads(obj["code"].encode())
+        globs = Deserializer.deserialize_dict(obj["globals"])
+
+        for i in obj["libs"]:
+            globs[i] = importlib.import_module(i)
+
+        return types.FunctionType(code, globs, obj["name"])
+
+    @staticmethod
+    def deserialize_class(obj: dict) -> object:
+        return type(obj["name"], tuple(Deserializer.deserialize_list(obj["bases"])),
+                    Deserializer.deserialize_dict(obj["attributes"]) | Deserializer.deserialize_dict(obj["methods"]))
+
+    @staticmethod
+    def deserialize_class_object(obj: dict) -> object:
+        s = Deserializer.loads(obj["class"]).__new__(type(Deserializer.deserialize_dict(obj["class"])))
+        for i in Deserializer.deserialize_dict(obj["attributes"]).items():
+            s.__setattr__(i[0], i[1])
+
+        return s
+
+    @staticmethod
+    def deserialize_dict(obj: dict) -> dict or type:
+
+        if obj.get("type") is not None:
+            if obj["type"] == "function":
+                return Deserializer.deserialize_function(obj)
+
+            if obj["type"] == "class":
+                return Deserializer.deserialize_class(obj)
+
+            if obj["type"] == "object":
+                return Deserializer.deserialize_class_object(obj)
+
+        s = {}
+        for key, value in obj.items():
+            s[key] = Deserializer.loads(value)
+
+        return dict(s)
+
+    @staticmethod
+    def deserialize_list(obj) -> list:
+        s = []
+
+        for i in obj:
+            s.append(Deserializer.loads(i))
+
+        return s
+
+    @staticmethod
+    def deserialize_set(obj) -> set:
+        s = set()
+
+        for i in obj:
+            s.add(Deserializer.loads(i))
+
+        return s
+
+    @staticmethod
+    def loads(obj) -> object:
+        if type(obj) is int or type(obj) is float or type(obj) is bool or type(obj) is str:
+            return obj
+
+        elif type(obj) is list:
+            return Deserializer.deserialize_list(obj)
+
+        elif type(obj) is set:
+            return Deserializer.deserialize_set(obj)
+
+        elif type(obj) is dict:
+            return Deserializer.deserialize_dict(obj)
--- lab2/main.py
+++ lab2/main.py
@@ -1,23 +1,2 @@
 import Json
-import math
 
-c = 2
-
-
-def f():
-    x = 2
-    return math.sin(x)
-
-
-class Cat:
-    def __init__(self, name: str):
-        self.name = name
-
-    def pet(self) -> str:
-        return "u pet a " + self.name
-
-
-cat = Cat("l")
-s = (2, 3)
-
-print(Json.Serializer.dumps(cat))
